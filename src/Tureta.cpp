087072bb3a116e7b05d79650cd0d443ebff15392
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2020-07-17 11:13:33 +0300

    Only initialize the used timers

--- src/Tureta.cpp
+++ src/Tureta.cpp
@@ -8,6 +8,39 @@
 #define joyDeadzone 15
 #define NUM_AXIS 2
 
+int16_t joyIdlePos = 512;
+ShortTimer analogUpdateTimer;
+
+static void timerInit(uint8_t timer)
+{
+    CRITICAL_SECTION_START;
+    switch (timer)
+    {
+        #if defined(TCCR1B) && defined(CS11) && defined(WGM10) && defined(WGM13)
+        case 1:
+            cbi(TCCR1B, CS11);    // set timer 1 prescale factor to 1
+            sbi(TCCR1B, CS10);
+            sbi(TCCR1A, WGM13);   // put timer 1 in 8-bit phase and frequency correct pwm mode
+        break;
+        #endif
+        #if defined(TCCR3B) && defined(CS31) && defined(WGM30) && defined(WGM33)
+        case 3:
+            cbi(TCCR3B, CS31);    // set timer 3 prescale factor to 1
+            sbi(TCCR3B, CS30);
+            sbi(TCCR3A, WGM33);   // put timer 3 in 8-bit phase and frequency correct pwm mode
+        break;
+        #endif
+        #if defined(TCCR4B) && defined(CS41) && defined(WGM40) && defined(WGM43)
+        case 4:
+            cbi(TCCR4B, CS41);    // set timer 4 prescale factor to 1
+            sbi(TCCR4B, CS40);
+            sbi(TCCR4A, WGM43);   // put timer 4 in 8-bit phase and frequency correct pwm mode
+        break;
+        #endif
+    }
+    CRITICAL_SECTION_END;
+}
+
 class axis_t
 {
 public:
@@ -32,7 +65,6 @@ private:
     int8_t en_pin;
     int8_t joy_pin;
     int8_t dir_flip;
-    
     uint8_t timer;
 };
 
@@ -41,9 +73,6 @@ axis_t axis[NUM_AXIS] =
     {0, 8, -1, A2, 1, 3},
     {2, 7, -1, A3, 1, 4},
 };
-
-int16_t joyIdlePos = 511;
-ShortTimer analogUpdateTimer;
 
 int8_t axis_t::joyToDirection(int16_t value)
 {
@@ -63,6 +92,7 @@ uint16_t axis_t::joyToSpeed(int16_t value)
 
 void axis_t::init()
 {
+    timerInit(timer);
     pinMode(step_pin, OUTPUT); digitalWrite(step_pin, LOW);
     pinMode(dir_pin, OUTPUT); digitalWrite(dir_pin, LOW);
     pinMode(en_pin, OUTPUT); digitalWrite(en_pin, LOW);
@@ -92,31 +122,7 @@ void axis_t::process()
     Serial.println(outText);
 }
 
-void timerInit()
-{
-    CRITICAL_SECTION_START;
-/* 
-#if defined(TCCR1B) && defined(CS11) && defined(WGM10) && defined(WGM13)
-    cbi(TCCR1B, CS11);    // set timer 1 prescale factor to 1
-    sbi(TCCR1B, CS10);
-    sbi(TCCR1A, WGM13);   // put timer 1 in 8-bit phase and frequency correct pwm mode
-#endif
- */
-#if defined(TCCR3B) && defined(CS31) && defined(WGM30) && defined(WGM33)
-    cbi(TCCR3B, CS31);    // set timer 3 prescale factor to 1
-    sbi(TCCR3B, CS30);
-    sbi(TCCR3A, WGM33);   // put timer 3 in 8-bit phase and frequency correct pwm mode
-#endif
-#if defined(TCCR4B) && defined(CS41) && defined(WGM40) && defined(WGM43)
-  cbi(TCCR4B, CS41);    // set timer 4 prescale factor to 1
-  sbi(TCCR4B, CS40);
-  sbi(TCCR4A, WGM43);   // put timer 4 in 8-bit phase and frequency correct pwm mode
-#endif
-    CRITICAL_SECTION_END;
-}
-
 void setup() {
-    timerInit();
     Serial.begin(115200);
     UCSR0B &= ~(1<<RXEN0);
     Serial.println(F("start"));
