16a3ccde415b9e229e72da32e9ae2f2acd5db7cc
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2020-12-19 13:37:55 +0200

    Change stepper speeds

--- src/Tureta.cpp
+++ src/Tureta.cpp
@@ -95,7 +95,7 @@ direction_t axis_t::joyToDirection(int16_t value)
 
 uint16_t axis_t::joyToSpeed(int16_t value)
 {
-    return calc_timer(map(abs(value - joyIdlePos), joyDeadzone, joyIdlePos, 32, 10000));
+    return calc_timer(map(abs(value - joyIdlePos), joyDeadzone, joyIdlePos, 32, 2000));
 }
 
 void axis_t::init()
